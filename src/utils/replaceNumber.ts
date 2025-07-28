2377093cd00f8fe995341b74ba2c90054e8b65b1
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-28 12:36:28 +0330

    replaceNumber util added

---
+++ src/utils/replaceNumber.ts
@@ -0,0 +1,18 @@
+const e2p = (s: string): string =>
+  s.toString().replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[parseInt(d)]);
+
+const p2e = (s: string | number): string =>
+  s.toString().replace(/[۰-۹]/g, (d) => String("۰۱۲۳۴۵۶۷۸۹".indexOf(d)));
+
+const sp = (number: string | number): string => {
+  const separatedNumber = number
+    .toString()
+    .match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
+
+  if (!separatedNumber) return "";
+
+  const joinedNumber = separatedNumber.join(",");
+  return e2p(joinedNumber);
+};
+
+export { e2p, p2e, sp };
