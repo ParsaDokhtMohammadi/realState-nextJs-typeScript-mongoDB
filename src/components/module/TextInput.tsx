a22f9a894778331f13e2c12e9d3ff97d84b0ed04
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-28 12:29:35 +0330

    textInput module created

---
+++ src/components/module/TextInput.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import styles from "@/components/module/TextInput.module.css"
+import { IProfile } from '@/types/Interfaces'
+
+interface ITextInput {
+    title : string
+    name : keyof IProfile
+    profileData : IProfile
+    setProfileData : Dispatch<SetStateAction<IProfile>>
+    textArea : boolean
+}
+
+const TextInput = ({title,name,profileData,setProfileData,textArea=false}:ITextInput) => {
+    const changeHandler = (e:React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) =>{
+        const {name , value} = e.target
+        setProfileData({...profileData , [name]:value})
+    }
+  return (
+    <div className={styles.container}>
+        <p>{title}</p>
+        {textArea ?(
+        <textarea name={name} value={profileData[name] as string}
+        onChange={changeHandler}
+        ></textarea>
+        ):(
+        <input type='text' name={name} value={profileData[name] as string}
+        onChange={changeHandler}
+        ></input>
+    )}
+    </div>
+  )
+}
+
+export default TextInput
