fcf862aa1e60d92663bcb241aac9247aa940aca8
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-29 17:35:13 +0330

    addProfilePage completed

--- src/components/template/AddProfilePage.tsx
+++ src/components/template/AddProfilePage.tsx
@@ -1,88 +1,99 @@
 "use client"
 import styles from "@/components/template/AddProfilePage.module.css"
 import { useState } from "react"
-import { CATEGORY, IProfile } from "@/types/Interfaces"
+import { CATEGORY, IProfile, IResponse } from "@/types/Interfaces"
 import TextInput from "../module/TextInput"
 import RadioList from "../module/RadioList"
 import TextList from "../module/TextList"
 import CustomDatePicker from "../module/CustomDatePicker"
-
+import toast, { Toaster } from "react-hot-toast"
+import { BeatLoader } from "react-spinners" 
 const AddProfilePage = () => {
-    const [profileData , setProfileData]=useState<IProfile>({
-        title:"",
-        description:"",
-        location:"",
-        phone:"",
-        price:"",
-        realState:"",
-        constructionDate : new Date(),
-        category:CATEGORY.NULL,
-        rules : [],
-        amenities : []
+  const [profileData, setProfileData] = useState<IProfile>({
+    title: "",
+    description: "",
+    location: "",
+    phone: "",
+    price: "",
+    realState: "",
+    constructionDate: new Date(),
+    category: CATEGORY.NULL,
+    rules: [],
+    amenities: []
+  })
+  const [loading , setLoading] = useState<boolean>(false)
+  const submitHandler = async (): Promise<void> => {
+    setLoading(true)
+    const res = await fetch("/api/profile", {
+      method: "POST",
+      body: JSON.stringify(profileData),
+      headers: { "Content-Type": "application/json" }
     })
-    const submitHandler = async():Promise<void>=>{
-      const res = await fetch("/api/profile",{
-        method:"POST",
-        body:JSON.stringify(profileData),
-        headers:{"Content-Type":"application/json"}
-      })
-      const data = await res.json()
-      if(data.error){
-        console.log("shit",data);
-      }else{
-        console.log("we did it",data);
-      }
+    const data:IResponse = await res.json()
+    if (data.error) {
+      toast.error(data.error)
+    } else {
+      toast.success(data.message||"")
     }
+    setLoading(false)
+  }
   return (
-    <div className={styles.container}>
-      <h3>ثبت آگهی</h3>
-      <TextInput 
-      title="عنوان آگهی" 
-      name="title" 
-      profileData={profileData} 
-      setProfileData={setProfileData}></TextInput>
-      <TextInput 
-      title="توضیحات"
-      name="description" 
-      profileData={profileData} 
-      setProfileData={setProfileData}
-      textArea={true}></TextInput> 
-      <TextInput 
-      title="آدرس" 
-      name="location" 
-      profileData={profileData} 
-      setProfileData={setProfileData}></TextInput>
-      <TextInput 
-      title="شماره تماس" 
-      name="phone" 
-      profileData={profileData} 
-      setProfileData={setProfileData}></TextInput>
-      <TextInput 
-      title="قیمت(تومان)" 
-      name="price" 
-      profileData={profileData} 
-      setProfileData={setProfileData}></TextInput>
-      <TextInput 
-      title="بنگاه" 
-      name="realState" 
-      profileData={profileData} 
-      setProfileData={setProfileData}></TextInput>
-      <RadioList profileData={profileData} setProfileData={setProfileData}></RadioList>
-      <TextList
-      title="امکانات رفاهی" 
-      profileData={profileData}
-      setProfileData={setProfileData} 
-      type="ameneties"
-      ></TextList>
-      <TextList
-      title="قوانین" 
-      profileData={profileData}
-      setProfileData={setProfileData} 
-      type="roles"
-      ></TextList>
-      <CustomDatePicker profileData={profileData} setProfileData={setProfileData}/>
-      <button className={styles.submit} onClick={submitHandler}>ثبت آگهی</button>
-    </div>
+    <>
+      <Toaster></Toaster>
+      <div className={styles.container}>
+        <h3>ثبت آگهی</h3>
+        <TextInput
+          title="عنوان آگهی"
+          name="title"
+          profileData={profileData}
+          setProfileData={setProfileData}></TextInput>
+        <TextInput
+          title="توضیحات"
+          name="description"
+          profileData={profileData}
+          setProfileData={setProfileData}
+          textArea={true}></TextInput>
+        <TextInput
+          title="آدرس"
+          name="location"
+          profileData={profileData}
+          setProfileData={setProfileData}></TextInput>
+        <TextInput
+          title="شماره تماس"
+          name="phone"
+          profileData={profileData}
+          setProfileData={setProfileData}></TextInput>
+        <TextInput
+          title="قیمت(تومان)"
+          name="price"
+          profileData={profileData}
+          setProfileData={setProfileData}></TextInput>
+        <TextInput
+          title="بنگاه"
+          name="realState"
+          profileData={profileData}
+          setProfileData={setProfileData}></TextInput>
+        <RadioList profileData={profileData} setProfileData={setProfileData}></RadioList>
+        <TextList
+          title="امکانات رفاهی"
+          profileData={profileData}
+          setProfileData={setProfileData}
+          type="ameneties"
+        ></TextList>
+        <TextList
+          title="قوانین"
+          profileData={profileData}
+          setProfileData={setProfileData}
+          type="roles"
+        ></TextList>
+        <CustomDatePicker profileData={profileData} setProfileData={setProfileData} />
+       {loading ?(
+        <BeatLoader color='#304ffe' size={25} cssOverride={{margin:"auto"}}/>
+       ):(
+         <button className={styles.submit} onClick={submitHandler}>ثبت آگهی</button>
+       )}
+      </div>
+    </>
   )
 }
 
