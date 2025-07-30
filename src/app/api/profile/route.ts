760c02e293427adc135af030a18967dafb65b191
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-30 19:51:24 +0330

    profile PATCH method added

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -6,6 +6,8 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import User from "@/models/User";
 import Profile from "@/models/Profile";
 import { Types } from "mongoose";
+import { error } from "console";
+
 
 
 export async function POST(req:NextRequest) {
@@ -22,7 +24,7 @@ export async function POST(req:NextRequest) {
         }
         const user:IUser|null = await User.findOne({email:session.user?.email})
         if(!user){
-            return NextResponse.json({error:"کاربر یافت نشد",status:401})
+            return NextResponse.json({error:"کاربر یافت نشد",status:404})
         }
         if(
             !title||!description||!location||
@@ -40,6 +42,55 @@ export async function POST(req:NextRequest) {
      }
     catch(err){
         console.log(err);
+        return NextResponse.json(
+            {error:"مشکلی در سرور رخ داده است",status:500}
+        )
+    }
+}
+export async function PATCH(req:NextResponse) {
+    try{
+        await connectDB()
+        const body:IProfile = await req.json() 
+        const {
+            _id,title , description , location , 
+            phone , price ,realState , constructionDate
+            ,ameneties , rules , category} = body
+        const session = await getServerSession(authOptions)
+        if(!session) {
+            return NextResponse.json({error:"لطفا وارد حساب کاربری خود شوید",status : 401})
+        }
+         const user:IUser|null = await User.findOne({email:session.user?.email})
+        if(!user){
+            return NextResponse.json({error:"کاربر یافت نشد",status:404})
+        }
+        if(
+            !_id||!title||!description||!location||
+            !phone||!price||!realState||
+            !constructionDate||!category
+        ) return NextResponse.json({error:"اطلاعات نامعتبر است",status:400})
+        const profile:IProfile|null = await Profile.findOne({_id:_id})
+        
+        if (!profile) {
+        return NextResponse.json({ error: "آگهی یافت نشد", status: 404 });
+        }
+        if(!user._id.equals(profile?.userId)){
+            return NextResponse.json({error:"دست رسی شما به این آگهی محدود شده است",status:403})
+        }
+        profile.title = title
+        profile.description = description
+        profile.location = location
+        profile.phone = phone
+        profile.realState = realState
+        profile.price = price
+        profile.constructionDate = constructionDate
+        profile.ameneties = ameneties
+        profile.rules = rules
+        profile.category = category
+        profile.save()
+        return NextResponse.json({message:"آگهی با موفقیت ویرایش شد",status:200})
+    }
+    catch(err){
+         console.log(err);
         return NextResponse.json(
             {error:"مشکلی در سرور رخ داده است",status:500}
         )
