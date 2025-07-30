9f104a2f0d2fb14262b08e77cb7f974d579e2ac8
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-30 12:14:53 +0330

    Card component completed

--- src/components/module/Card.tsx
+++ src/components/module/Card.tsx
@@ -1,9 +1,40 @@
+import { JSX } from "react"
 import styles from "@/components/module/Card.module.css"
-import { IProfile } from "@/types/Interfaces"
-const Card = ({data}:{data:IProfile}) => {
-  return (
-    <div>
+import { CATEGORY, IProfile } from "@/types/Interfaces"
+import { RiHome3Line } from "react-icons/ri"
+import { BiStore } from "react-icons/bi"
+import { MdApartment } from "react-icons/md"
+import { GiOfficeChair } from "react-icons/gi"
+import { FaQuestionCircle } from "react-icons/fa"
+import { HiOutlineLocationMarker } from "react-icons/hi"
+import { BiLeftArrowAlt } from "react-icons/bi"
+import { sp } from "@/utils/replaceNumber"
+import Link from "next/link"
 
+
+const Card = ({data : {category,title,location,price}}:{data:IProfile}) => {
+const icons: Record<CATEGORY, JSX.Element> = {
+  [CATEGORY.VILLA]: <RiHome3Line />,
+  [CATEGORY.APARTMENT]: <MdApartment />,
+  [CATEGORY.STORE]: <BiStore />,
+  [CATEGORY.OFFICE]: <GiOfficeChair />,
+  [CATEGORY.NULL]: <FaQuestionCircle />
+}
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>
+        {icons[category]}
+        <p className={styles.title}>{title}</p>
+        <p className={styles.location}>
+          <HiOutlineLocationMarker/>
+          {location}
+        </p>
+        <span>{sp(price) }  تومان</span>
+        <Link href={"/"}>مشاهده آگهی
+        <BiLeftArrowAlt/>
+        </Link>
+
+      </div>
     </div>
   )
 }
