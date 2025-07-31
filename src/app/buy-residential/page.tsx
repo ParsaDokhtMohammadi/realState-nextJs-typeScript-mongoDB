5a305f8297681000e4683706f836cd0c42576d92
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-31 16:24:56 +0330

    category based filtering added

--- src/app/buy-residential/page.tsx
+++ src/app/buy-residential/page.tsx
@@ -1,16 +1,20 @@
 import BuyResidentialsPage from '@/components/template/BuyResidentialsPage'
 import React from 'react'
-import { IProfile } from "@/types/Interfaces"
+import { CATEGORY, IProfile } from "@/types/Interfaces"
 import connectDB from '@/utils/connectDB'
 import Profile from '@/models/Profile'
 
 
-const BuyResidentials = async() => {
+const BuyResidentials = async({searchParams}:{searchParams:{category:CATEGORY}}) => {
+  
   await connectDB()
   const data: IProfile[] = await Profile.find()
-  
+  let finalData = data
+  if (searchParams.category){
+   finalData = finalData.filter(datum=>datum.category===searchParams.category)
+  }
   return (
-    <BuyResidentialsPage data={data}></BuyResidentialsPage>
+    <BuyResidentialsPage data={finalData}></BuyResidentialsPage>
   )
 }
 
--- src/components/module/CategoryCard.tsx
+++ src/components/module/CategoryCard.tsx
@@ -1,12 +1,14 @@
 import styles from "@/components/module/CategoryCard.module.css"
+import { CATEGORY } from "@/types/Interfaces"
 import Image from "next/image"
 import Link from "next/link"
 
 const CategoryCard = ({name , title}:{name:string,title:string}) => {
   return (
     <div className={styles.card}>
-        <Link href="/"></Link>
+        <Link href={`/buy-residential?category=${name}`}>
         <Image src={`/images/${name}.png`} alt={title} width={240} height={144} priority={true}/>
+        </Link>
         <p>{title}</p>
     </div>
   )
