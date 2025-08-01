ba262f540e252c3fc1b8df1021087b2d96c8b452
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-08-01 17:16:56 +0330

    adminCard completed

--- src/components/module/AdminCard.tsx
+++ src/components/module/AdminCard.tsx
@@ -1,16 +1,35 @@
+"use client"
 import styles from "@/components/module/AdminCard.module.css"
-import { IProfile } from "@/types/Interfaces"
+import { IProfile, IResponse } from "@/types/Interfaces"
 import { sp } from "@/utils/replaceNumber"
+import { useRouter } from "next/navigation"
+import { Toaster , toast } from "react-hot-toast"
 const AdminCard = ({data} : {data:IProfile}) => {
+    const router = useRouter()
+    const publishHandler = async() =>{
+        const res = await fetch(`/api/published/${data._id}`,{
+            method:"PATCH"
+        })
+        const messageData:IResponse = await res.json()
+        if(messageData.message){
+            toast.success(messageData.message)
+            
+        }
+        else if(messageData.error){
+            toast.error(messageData.error) 
+        }
+        router.refresh()
+    }
   return (
     <div className={styles.container}>
+        <Toaster/>
         <h3>{data.title}</h3>
         <p>{data.description}</p>
         <div className={styles.properties}>
             <span>{data.location}</span>
             <span>{sp(data.price)}</span>
         </div>
-        <button>اتشار</button>
+        <button onClick={publishHandler}>اتشار</button>
     </div>
   )
 }
