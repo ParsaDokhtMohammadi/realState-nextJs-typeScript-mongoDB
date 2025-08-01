552fb95834fa473667bccafe09247fe330e00085
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-08-01 11:29:48 +0330

    detailsPage sidebar created

--- src/components/template/DetailsPage.tsx
+++ src/components/template/DetailsPage.tsx
@@ -9,7 +9,21 @@ import { BiStore } from "react-icons/bi"
 import { GiOfficeChair } from "react-icons/gi"
 import { BiCalendarCheck } from "react-icons/bi"
 import ItemList from "../module/ItemList"
+import { e2p, sp } from "@/utils/replaceNumber"
+import { JSX } from "react"
 
+const categories:{villa:string,apartment:string,store:string,office:string} = {
+  apartment : "آپارتمان",
+  villa : "ویلا",
+  store : "مغازه",
+  office : "دفتر"
+}
+const icons:{villa:JSX.Element,apartment:JSX.Element,store:JSX.Element,office:JSX.Element}={
+  villa : <RiHome3Line/>,
+  apartment : <MdApartment/>,
+  store : <BiStore/>,
+  office : <GiOfficeChair/>
+}
 
 const DetailsPage = ({ data }: { data: IProfile }) => {
   console.log(data);
@@ -30,7 +44,28 @@ const DetailsPage = ({ data }: { data: IProfile }) => {
         <ItemList data={data.rules} />
 
       </div>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <div className={styles.realState}>
+          <SiHomebridge/>
+          <p>املاک {data.realState}</p>
+          <span>
+            <AiOutlinePhone/>
+            {e2p(data.phone)}
+          </span>
+        </div>
+        <div className={styles.price}>
+          <p>
+            {icons[data.category as keyof typeof icons]}
+          </p>
+          <p>
+            {sp(data.price)} تومان 
+          </p>
+          <p>
+            <BiCalendarCheck/>
+            {new Date(data.constructionDate).toLocaleDateString("fa-IR")}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
